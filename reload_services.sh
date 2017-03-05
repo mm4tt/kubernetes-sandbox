3ebc383a28222139c3418cf7e74859a90f038efc
Mateusz Matejczyk <mateusz.matejczyk@gmail.com>
2017-03-04 19:17:50 -0500

    Reload services script.

---
+++ reload_services.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+tag=`date +%Y%m%d%H%M%S`
+
+gradle backendJar &&
+docker build -f src/main/docker/backend -t gcr.io/docker-sandbox-144501/backend:$tag . &&
+gcloud docker -- push gcr.io/docker-sandbox-144501/backend:$tag &&
+kubectl set image deployment/backend backend=gcr.io/docker-sandbox-144501/backend:$tag
+
+
+gradle frontendJar &&
+docker build -f src/main/docker/frontend -t gcr.io/docker-sandbox-144501/frontend:$tag . &&
+gcloud docker -- push gcr.io/docker-sandbox-144501/frontend:$tag &&
+kubectl set image deployment/backend backend=gcr.io/docker-sandbox-144501/frontend:$tag
+g
