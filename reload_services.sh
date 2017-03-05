433fb4f958d60ee7846ef1b357531bd6c4d1b3fa
Mateusz Matejczyk <mateusz.matejczyk@gmail.com>
2017-03-04 20:10:41 -0500

    Actually push the latest image to the gcloud docker repo.

--- reload_services.sh
+++ reload_services.sh
@@ -12,3 +12,7 @@ gradle frontendJar &&
 docker build -f src/main/docker/frontend -t gcr.io/docker-sandbox-144501/frontend -t gcr.io/docker-sandbox-144501/frontend:$tag . &&
 gcloud docker -- push gcr.io/docker-sandbox-144501/frontend:$tag &&
 kubectl set image deployment/frontend frontend=gcr.io/docker-sandbox-144501/frontend:$tag
+
+gcloud docker -- push gcr.io/docker-sandbox-144501/backend:$tag
+gcloud docker -- push gcr.io/docker-sandbox-144501/frontend:$tag
+
