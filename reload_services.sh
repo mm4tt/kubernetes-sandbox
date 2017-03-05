15c0494a57dffcb1ed9a2ec2bc309157fdad3f67
Mateusz Matejczyk <mateusz.matejczyk@gmail.com>
2017-03-04 19:27:50 -0500

    In reload_services.sh set also latest tag.

--- reload_services.sh
+++ reload_services.sh
@@ -3,13 +3,12 @@
 tag=`date +%Y%m%d%H%M%S`
 
 gradle backendJar &&
-docker build -f src/main/docker/backend -t gcr.io/docker-sandbox-144501/backend:$tag . &&
+docker build -f src/main/docker/backend -t gcr.io/docker-sandbox-144501/backend -t gcr.io/docker-sandbox-144501/backend:$tag . &&
 gcloud docker -- push gcr.io/docker-sandbox-144501/backend:$tag &&
 kubectl set image deployment/backend backend=gcr.io/docker-sandbox-144501/backend:$tag
 
 
 gradle frontendJar &&
-docker build -f src/main/docker/frontend -t gcr.io/docker-sandbox-144501/frontend:$tag . &&
+docker build -f src/main/docker/frontend -t gcr.io/docker-sandbox-144501/frontend -t gcr.io/docker-sandbox-144501/frontend:$tag . &&
 gcloud docker -- push gcr.io/docker-sandbox-144501/frontend:$tag &&
-kubectl set image deployment/backend backend=gcr.io/docker-sandbox-144501/frontend:$tag
-g
+kubectl set image deployment/frontend frontend=gcr.io/docker-sandbox-144501/frontend:$tag
